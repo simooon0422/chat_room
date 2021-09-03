1176cccfa36e5c974c7f1a580a85a4d031eabb05
Szymon Hudziak <szymonh.dg@gmail.com>
2021-09-03 14:19:37 +0200

    Added simple GUI

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 import socket
 import threading
 import random
-import string
 from random import randint
+import string
 from time import sleep
+import tkinter
+import tkinter.scrolledtext
 
 
 class ChatClient:
@@ -12,32 +14,84 @@ class ChatClient:
         self.port = port
         self.nickname = nickname
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.gui_done = False
+        self.running = True
 
     def run_client(self):
         """Starting client"""
         # Connecting To Server
         self.client.connect((self.host, self.port))
         # Starting Threads For Listening And Writing
+        gui_thread = threading.Thread(target=self._run_gui)
+        gui_thread.start()
+
         receive_thread = threading.Thread(target=self._receive)
         receive_thread.start()
 
-        write_thread = threading.Thread(target=self._write)
-        write_thread.start()
+        # write_thread = threading.Thread(target=self._write)
+        # write_thread.start()
 
-        simulation_thread = threading.Thread(target=self._simulate_chat)
-        simulation_thread.start()
+        # simulation_thread = threading.Thread(target=self._simulate_chat)
+        # simulation_thread.start()
+
+    def _run_gui(self):
+        """Creating GUI"""
+        self.window = tkinter.Tk()
+
+        # self.window.geometry('600x800')
+        self.window.title('Chat Room')
+
+        self.chat_label = tkinter.Label(self.window, text='Chat', font=('Arial', 24, 'bold'), padx=20, pady=10)
+        self.chat_label.pack()
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.window, padx=20, pady=10, state='disabled')
+        self.text_area.pack()
+
+        self.message_label = tkinter.Label(self.window, text='Message', font=('Arial', 24, 'bold'), padx=20, pady=10)
+        self.message_label.pack()
+
+        self.entry = tkinter.Text(self.window, height=3, padx=20, pady=5)
+        self.entry.pack()
+
+        # self.entry = tkinter.Entry(self.window, font=('Arial', 12))
+        # self.entry.pack()
+
+        self.send_button = tkinter.Button(self.window,
+                                          text='SEND',
+                                          command=self._write,
+                                          font=('Arial', 24),
+                                          padx=20,
+                                          pady=10)
+        self.send_button.pack()
+
+        self.gui_done = True
+
+        self.window.protocol('WM_DELETE_WINDOW', self._stop)
+
+        self.window.mainloop()
+
+    def _stop(self):
+        """Stopping program"""
+        self.running = False
+        self.window.destroy()
+        self.client.close()
+        exit(0)
 
     def _receive(self):
         """Listening to Server and Sending Nickname"""
-        while True:
+        while self.running:
             try:
                 # Receive Message From Server
                 # If 'NICK' Send Nickname
                 message = self.client.recv(1024).decode()
                 if message == 'NICK':
                     self.client.send(self.nickname.encode())
-                else:
-                    print(message, end='')
+                elif self.gui_done:
+                    self.text_area.config(state='normal')
+                    self.text_area.insert('end', message)
+                    self.text_area.yview('end')
+                    self.text_area.config(state='disabled')
+                    # print(message, end='')
 
             except:
                 # Close Connection When Error
@@ -47,9 +101,9 @@ class ChatClient:
 
     def _write(self):
         """Writing messages"""
-        while True:
-            message = f'{self.nickname}: {input("")}'
-            self.client.send(message.encode())
+        message = f'{self.nickname}: {self.entry.get("1.0", "end")}'
+        self.client.send(message.encode())
+        self.entry.delete('1.0', 'end')
 
     def _simulate_chat(self):
         """Writing random messages"""
@@ -72,4 +126,4 @@ def chat_client(clients_number=1):
         ChatClient('127.0.0.1', 8081, f'nick_{str(index)}').run_client()
 
 
-chat_client(10)
+chat_client(2)
